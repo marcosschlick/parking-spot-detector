72e897a3231c9c1933e0ab538df05499029c34bf
Marcos Schlick <marcosschlick@gmail.com>
2025-08-11 13:49:06 -0300

    remover pacote não utilizado

--- src/test.py
+++ src/test.py
@@ -1,9 +1,7 @@
 import cv2
-import numpy as np
 from ultralytics import __version__ as ultralytics_version
 from sklearn import __version__ as sklearn_version
 
 print("OpenCV version:", cv2.__version__)
-print("NumPy version:", np.__version__)
 print("Ultralytics YOLO version:", ultralytics_version)
 print("Scikit-learn version:", sklearn_version)
