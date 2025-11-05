4fdad26286b0f436af42ce2951581a25eef47d9d
Marcos Schlick <marcosschlick@gmail.com>
2025-11-05 16:04:06 -0300

    add python files to train model with different versions of YOLO from 8 to 12

---
+++ src/scripts/train_model_v10.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Load a pretrained model
+model = YOLO("yolo10n.pt")
+
+# Train the model on your custom dataset
+model.train(data="config.yaml", epochs=5, imgsz=640)   
