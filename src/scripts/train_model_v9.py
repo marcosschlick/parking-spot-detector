80500ae61c56549916190f166e894dd2e2089fc6
Marcos Schlick <marcosschlick@gmail.com>
2025-11-06 19:36:02 -0300

    fix YOLO model name across different versions

--- src/scripts/train_model_v9.py
+++ src/scripts/train_model_v9.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a pretrained model
-model = YOLO("yolo9n.pt")
+model = YOLO("yolov9t.pt")
 
 # Train the model on your custom config
 model.train(cfg="config.yaml", project="results/yolo_v9")   
