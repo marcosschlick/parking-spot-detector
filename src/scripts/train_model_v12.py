b2d3451a22fe5161c17139550aee27163b7937b2
Marcos Schlick <marcosschlick@gmail.com>
2025-11-06 14:51:57 -0300

    remove file that will not be used

--- src/scripts/train_model_v12.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a pretrained model
-model = YOLO("yolo12n.pt")
-
-# Train the model on your custom dataset
-model.train(data="config.yaml", epochs=5, imgsz=640)   
